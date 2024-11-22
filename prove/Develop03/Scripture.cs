23f69d674a20697947083e2b6fdb874318fee35e
jkulmus <jacquelynkulmus@gmail.com>
2024-11-22 15:30:56 -0800

    Week 3

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+        foreach (string word in text.Split(' '))
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random random = new Random();
+        for (int i = 0; i < numberToHide; i++)
+        {
+            int index = random.Next(_words.Count);
+            _words[index].Hide();
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string displayText = _reference.GetDisplayText() + "\n";
+        foreach (Word word in _words)
+        {
+            displayText += word.GetDisplayText() + " ";
+        }
+        return displayText.TrimEnd();
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+                return false;
+        }
+        return true;
+    }
+}
